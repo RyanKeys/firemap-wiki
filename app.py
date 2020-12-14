3b64f0770226b2fb8ff699e0af0e4d38b3a1e4b5
Ryan Keys <r.keys1998@gmail.com>
2020-12-13 23:42:19 -0800

    aaa

--- app.py
+++ app.py
@@ -16,7 +16,23 @@ def getting_started():
 
 @app.route("/documentation")
 def documentation():
-    return render_template("documentation.html")
+    return render_template('documentation.html')
+
+@app.route("/documentation/<selection>")
+def read_selection(selection):
+    if str(selection).lower() == "coordinates":
+        context =  {
+            "name": "Coordinates",
+            "summary": "Displays the latitude and longitude of selected fire.",
+            "description": "Accessible thorugh the Active Fire Map GUI, or by creating an API key and going to the '/api/fires' endpoint.",
+        }
+    else:
+        context = {
+            "name": f"{selection}",
+            "summary": "pellentesque habitant morbi tristique senectus",
+            "description": """tempus quam pellentesque nec nam aliquam sem et tortor consequat id porta nibh venenatis cras sed felis eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque"""
+        }
+    return render_template('detail.html', context=context)
 
 if __name__ == "__main__":
     app.run()
